d890940560682149d8993467532643d2ab4f5fe2
Andres Olave <sloops77@users.noreply.github.com>
2025-04-02 05:08:42 +0300

    Refactor/local registrar app dev (#22)
    * fix: samples for credentialagent e2e
    
    Signed-off-by: Andres Olave <arolave@gmail.com>
    
    * fix: README.md
    
    Signed-off-by: Andres Olave <arolave@gmail.com>
    
    ---------
    
    Signed-off-by: Andres Olave <arolave@gmail.com>

--- samples/sample-web-wallet-app/.eslintrc.js
+++ samples/sample-web-wallet-app/.eslintrc.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const eslintConfig = require('../../../.eslintrc');
+const eslintConfig = require('../../.eslintrc');
 
 module.exports = {
   ...eslintConfig,
