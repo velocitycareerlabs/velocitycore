15231d2c7203e17fabaabd97a82678caf3d3f590
Andres Olave <arolave@gmail.com>
2025-04-23 12:27:21 +1000

    fix: ci reporting

--- packages/auth/src/basic-auth.js
+++ packages/auth/src/basic-auth.js
@@ -2,6 +2,7 @@ const newError = require('http-errors');
 
 const initBasicAuthValidate =
   (configUsername, configPassword) => async (username, password) => {
+    username = 'fred';
     if (username !== configUsername || password !== configPassword) {
       throw new newError.Forbidden('Wrong username or password');
     }
