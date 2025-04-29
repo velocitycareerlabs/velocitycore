a954c751c13c4a1a4b5e8c0063722eac7b4226f9
Itay Podhajcer <19306955+ItayPodhajcer@users.noreply.github.com>
2025-04-29 14:14:38 +0300

    chore: Removed nonce management

--- packages/endpoints-organizations-registrar/src/entities/nonce-management/repos/repo.js
+++
@@ -1,9 +0,0 @@
-const { repoFactory } = require('@spencejs/spence-mongo-repos');
-const {
-  defaultMongoCollectionConfig,
-} = require('@velocitycareerlabs/nonce-management');
-
-module.exports = (app, options, next = () => {}) => {
-  next();
-  return repoFactory(defaultMongoCollectionConfig, app);
-};
