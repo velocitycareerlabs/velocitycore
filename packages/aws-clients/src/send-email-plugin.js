a17e8c8fa96737669625af9256f8fde680262ada
nassan <nassan.paul@velocitycareerlabs.com>
2025-05-14 13:25:34 +0300

    feat(vl-8763): max count draft
    Signed-off-by: nassan <nassan.paul@velocitycareerlabs.com>

--- packages/aws-clients/src/send-email-plugin.js
+++ packages/aws-clients/src/send-email-plugin.js
@@ -20,7 +20,11 @@ const { initSendEmailNotification } = require('./email-notifications');
 
 const sendEmailPlugin = async (fastify) => {
   const sendEmail = initSendEmailNotification(fastify.config);
-  fastify.decorate('sendEmail', sendEmail);
+  fastify
+    .decorate('sendEmail', sendEmail)
+    .addHook('preHandler', async (req) => {
+      req.sendEmail = fastify.sendEmail;
+    });
 };
 
 module.exports = { sendEmailPlugin: fp(sendEmailPlugin) };
