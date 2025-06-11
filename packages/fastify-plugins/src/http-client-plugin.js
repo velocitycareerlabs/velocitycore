d9a5725d8fb77d4b95eaf091c525524299cf8407
Itay Podhajcer <19306955+ItayPodhajcer@users.noreply.github.com>
2025-06-11 17:02:51 +0300

    chore: Added http client plugin

---
+++ packages/fastify-plugins/src/http-client-plugin.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2025 Velocity Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+const { initHttpClient } = require('@velocitycareerlabs/http-client');
+const { capitalize } = require('lodash/fp');
+const fp = require('fastify-plugin');
+
+const httpClientPlugin = async (fastify, { name, options }) => {
+  const fastifyDecoration = `base${capitalize(name)}`;
+  const requestDecoration = name;
+  fastify
+    .decorate(fastifyDecoration, () => initHttpClient({ ...options, cache: fastify.cache }), ['cache'])
+    .decorateRequest(requestDecoration, null)
+    .addHook('preValidation', async (req) => {
+      req[requestDecoration] = fastify[fastifyDecoration](req);
+    });
+};
+
+module.exports = { httpClientPlugin: fp(httpClientPlugin) };
