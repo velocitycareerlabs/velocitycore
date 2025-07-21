7e0f9a52f61ea254071ff89dcde04bf3022080d1
Andres Olave <sloops77@users.noreply.github.com>
2025-07-21 17:32:51 +1000

    fix: fix nonce issue (#351)
    * chore: hack in log lines
    
    Signed-off-by: Andres Olave <arolave@gmail.com>
    
    * chore: no-console
    
    Signed-off-by: Andres Olave <arolave@gmail.com>
    
    * test: add wait between each test for nonce to settle
    
    Signed-off-by: Andres Olave <arolave@gmail.com>
    
    * test: create a default timeout value of 7s instead of 5s
    
    Signed-off-by: Andres Olave <arolave@gmail.com>
    
    * chore: remove console
    
    Signed-off-by: Andres Olave <arolave@gmail.com>
    
    * fix: add wait(1000) where needed
    
    Signed-off-by: Andres Olave <arolave@gmail.com>
    
    ---------
    
    Signed-off-by: Andres Olave <arolave@gmail.com>

--- packages/contract-permissions/test/permissions.test.js
+++ packages/contract-permissions/test/permissions.test.js
@@ -24,6 +24,7 @@ const {
   mongoCloseWrapper,
 } = require('@velocitycareerlabs/tests-helpers');
 const { env } = require('@spencejs/spence-config');
+const { wait } = require('@velocitycareerlabs/common-functions');
 const {
   deployTestPermissionsContract,
 } = require('./helpers/deploy-test-permissions-contract');
@@ -85,6 +86,10 @@ describe('Permissions Contract Test Suite', () => {
       permissioningAccount.privateKey,
       deployedContract
     );
+  });
+
+  afterEach(async () => {
+    await wait(1000);
   });
 
   afterAll(async () => {
