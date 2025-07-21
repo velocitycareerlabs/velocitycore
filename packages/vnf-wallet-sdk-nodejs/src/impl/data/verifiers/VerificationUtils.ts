b865ca6925e1cf2cd58b7eb3c5016cb8efd209d6
Michael Avoyan <64565419+michaelavoyan@users.noreply.github.com>
2025-07-21 09:53:18 +0300

    fix: fix lint

--- packages/vnf-wallet-sdk-nodejs/src/impl/data/verifiers/VerificationUtils.ts
+++ packages/vnf-wallet-sdk-nodejs/src/impl/data/verifiers/VerificationUtils.ts
@@ -26,7 +26,8 @@ export const getCredentialTypeMetadataByVc = (
 
 /**
  * The implementation relaying on the below reference:
- * https://github.com/velocitycareerlabs/velocitycore/blob/37c8535c2ef839ed72a2706685a398f20f4ae11c/packages/vc-checks/src/extract-credential-type.js#L20
+ * https://github.com/velocitycareerlabs/velocitycore
+ * /blob/37c8535c2ef839ed72a2706685a398f20f4ae11c/packages/vc-checks/src/extract-credential-type.js#L20
  */
 const getCredentialTypeName = (jwtVc: VCLJwt): string | undefined => {
     const types = jwtVc.payload?.vc?.type || [];
